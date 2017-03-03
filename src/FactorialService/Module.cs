4d54a8ba1b063612e698d09bd3e1e5ac3cacfedb
Jeremy Paddison <me@paddo.org>
2017-03-03 20:01:08 +0000

    added validation

--- src/FactorialService/Module.cs
+++ src/FactorialService/Module.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Logging;
 using Nancy;
 using MicroservicesDemo.FactorialService.Models;
+using Nancy.Validation;
 
 namespace MicroservicesDemo.FactorialService
 {
@@ -17,6 +18,13 @@ namespace MicroservicesDemo.FactorialService
 
         FactorialResponse GetFactorial(FactorialRequest request)
         {
+            var validationResult = this.Validate(request);
+            if (!validationResult.IsValid)
+            {
+                _logger.LogInformation($"Failed to generate factorial for {request.Number}");
+                return new FactorialResponse { Success = false, Errors = validationResult.FormattedErrors };
+            }
+            
             var result = new FactorialResponse { Success = true, Factorial = Factorial(request.Number) };
             _logger.LogInformation($"Generated Factorial for {request.Number}: {result.Factorial}");
 
